0036203f9d6b7717f177089662ca4b8b1204a132
Adwait Barkale <adwaitbarkale@gmail.com>
2025-01-24 14:38:18 +0530

    Worked on Home Screen UI

---
+++ CityGuide/src/Components/City.jsx
@@ -0,0 +1,79 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native'
+import React from 'react'
+import { HeartIcon } from 'react-native-heroicons/solid'
+import { useState } from 'react'
+
+const { width, height } = Dimensions.get('window');
+
+const City = () => {
+    const [favourite, setFavourite] = useState(false);
+    return (
+        <View style={{ alignItems: 'center' }}>
+            {/* City Element */}
+            <View style={styles.cityElement}>
+                {/* View Top */}
+                <View style={styles.viewTop}>
+                    <Image
+                        source={require('../Images/sampleCity.jpeg')}
+                        style={styles.cityImage}
+                    />
+                    <Text style={styles.cityName}>JAIPUR</Text>
+                    <TouchableOpacity style={styles.heartIcon} onPress={() => setFavourite(!favourite)}>
+                        <HeartIcon
+                            size="35"
+                            color={favourite ? 'red' : 'white'} />
+                    </TouchableOpacity>
+                </View>
+                {/* View Bottom */}
+                <View style={styles.viewBottom}>
+                    <Text style={{ textAlign: 'left', padding: 10 }}>
+                        Jaipur is the capital of India’s Rajasthan state. It evokes the royal family that once ruled the region and that, in 1727, founded what is now called the Old City, or “Pink City” for its trademark building color. At the center of its stately street grid (notable in India) stands the opulent, colonnaded City Palace complex. With gardens, courtyards and museums, part of it is still a royal residence.
+                    </Text>
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default City
+
+const styles = StyleSheet.create({
+    // City View
+    cityElement: {
+        // backgroundColor: 'green',
+        width: width * 0.9,
+        height: height * 0.4,
+        marginBottom: 20,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 20,
+    },
+    viewTop: {
+        // backgroundColor: 'green',
+        height: 0.5,
+        flex: 1
+    },
+    viewBottom: {
+        // backgroundColor: 'red',
+        height: 0.5,
+        flex: 1
+    },
+    cityImage: {
+        flex: 1,
+        width: '100%',
+        opacity: 1
+    },
+    cityName: {
+        color: 'white',
+        fontWeight: '900',
+        fontSize: 38,
+        position: 'absolute',
+        bottom: 10,
+        left: 2
+    },
+    heartIcon: {
+        position: 'absolute',
+        right: 10,
+        top: 10
+    }
+})
--- CityGuide/src/Screens/HomeScreen.jsx
+++ CityGuide/src/Screens/HomeScreen.jsx
@@ -1,15 +1,86 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Dimensions, StyleSheet, TextInput, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import City from '../Components/City';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const { width, height } = Dimensions.get('window');
 
 const HomeScreen = () => {
     const navigation = useNavigation();
-  return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Button title='Go to Detail' onPress={() => navigation.navigate('CityDetail')} />
-    </View>
-  )
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                {/* Search View */}
+                <View style={styles.searchView}>
+                    <View style={styles.inputContainer}>
+                        <Image
+                            source={require('../Images/icon_search.png')}
+                            style={styles.icons}
+                        />
+                        <TextInput
+                            placeholder='Search Cities...'
+                            style={styles.textInput} />
+                    </View>
+                    <View style={{ paddingRight: 6 }}>
+                        <TouchableOpacity>
+                            <Image
+                                source={require('../Images/icon_cross.png')}
+                                style={styles.icons}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+
+                {/* City List View*/}
+                <ScrollView style={styles.scrollView}>
+                    <City />
+                    <City />
+                    <City />
+                    <City />
+                </ScrollView>
+                {/* <Button title='Go to Detail' onPress={() => navigation.navigate('CityDetail')} /> */}
+            </View>
+        </SafeAreaView>
+    )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+    searchView: {
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        marginVertical: 20,
+        padding: 4,
+        width: width - 16,
+        marginHorizontal: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderRadius: 20
+    },
+    inputContainer: {
+        flex: 1,
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingLeft: 6
+    },
+    icons: {
+        width: 30,
+        height: 30
+    },
+    textInput: {
+        flex: 1,
+        marginHorizontal: 10,
+        fontSize: 16,
+        fontWeight: '500',
+        flexWrap: 'nowrap',
+        height: 50
+    },
+    scrollView: {
+        paddingBottom: 20,
+        flex: 1
+    }
+})
